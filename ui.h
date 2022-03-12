f32ae54dd7a6b31808b2203cdf483a6177b94630
Yan-huihui <100505176+Yan-huihui@users.noreply.github.com>
2022-03-12 22:00:15 +0800

    Add files via upload

---
+++ ui.h
@@ -0,0 +1,11 @@
+#ifndef __UI_
+#define __UI_
+
+
+extern void show_menu(void);
+extern void show_list(char (*p)[128], int list_bottom);
+extern void show_speed(void);
+extern void show_play_mode(void);
+extern void show_seek(void);
+
+#endif
